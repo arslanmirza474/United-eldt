b1b5969d5dddc4f73a2d6f5162bef479403e6ff7
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-02-11 00:55:09 +0500

    upa

--- src/Studentdashboard/Logout.jsx
+++ src/Studentdashboard/Logout.jsx
@@ -2,14 +2,14 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // Assuming you are using React Router
 
 function Logout() {
-  const history = useNavigate();
+  const navigate = useNavigate(); // Corrected to useNavigate()
 
   useEffect(() => {
     // Remove userId from local storage
     localStorage.removeItem('userId');
     // Redirect to login page or any other desired page
-    history('/login');
-  }, [history]);
+    navigate('/login'); // Corrected to use navigate
+  }, [navigate]);
 
   return null; // Since there's no content to render in the Logout component
 }
