192d9b9c0c47f51329cf22258c9a2545ff6d1d11
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-02-21 03:45:53 +0500

    yodate

--- src/components/Home/PopularCourses.jsx
+++ src/components/Home/PopularCourses.jsx
@@ -1,103 +1,23 @@
 import axios, { Axios } from "axios";
 import { Translator, Translate } from "react-auto-translate";
-import CourseCard from "./CourseCard";
 import { Link } from "react-router-dom";
-import { setLanguage } from "../../store/UserActions";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import CourseCardNew from "./CourseCardNew";
-import { useSelector } from "react-redux";
-import Select from 'react-select';
-import { useSpring, animated } from "react-spring";
+
 import "./courses.css"
 import { useState, useEffect, useRef } from 'react';
-import { Modal, Button, notification } from 'antd';
+import { Modal, notification } from 'antd';
 import google from "./images/google-pay-38 1.png"
 import apple from "./images/apple-logo-png-apple-mac-vector-logo-download-23 1.png"
 import Successi from "./images/Group 6674.png"
 import errir from "./images/Group 6674 (2).png"
 import { jwtDecode } from "jwt-decode";
-import {
-  CardNumberElement,
-  CardExpiryElement,
-  CardCvcElement,
-  useStripe,
-  useElements,
-  PaymentRequestButtonElement
-} from '@stripe/react-stripe-js';
 import CustomDropdown from "./CustomDropdown";
-import Buynew from "../../Studentdashboard/Buynew";
+import { useSelector } from "react-redux";
 
-
-
-const customStyles = {
-  control: (provided, state) => ({
-    ...provided,
-    minHeight: '50px', // Adjust the height of the input field
-  }),
-  menu: (provided, state) => ({
-    ...provided,
-    position: 'absolute !important',
-    zIndex: '9999 !important',
-    overflow: 'visible !important',
-    height: "fit-content !important"
-  }),
-  option: (provided, state) => ({
-    ...provided,
-    height: '35px !important',
-    overflow: 'visible !important',
-
-  }),
-};
-
-
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: "block",
-        background: "#FBB723",
-        width: "50px",
-        borderRadius: "50px",
-        height: "50px",
-        position: "absolute",
-        top: "50%",
-        right: "-94px",
-        right: 0,
-        transform: "translateY(-50%)",
-        zIndex: 1,
-        textAlign: "center",
-        lineHeight: "50px",
-        color: "white",
-      }}
-      onClick={onClick}
-    >
-      <span style={{ color: "black" }}>Hello</span>
-    </div>
-  );
-}
-
-function SamplepreArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "#FBB723", width: "50px", borderRadius: "50px", height: "50px" }}
-      onClick={onClick}
-    >Hello</div>
-  );
-}
 export default function PopularCourses({ language ,showCancelButton,handleNavigationClick,large,medium }) {
   const userState = useSelector((state) => state.user);
-  const [selectedLang, setSelected] = useState("all");
   const [loading, setLoading] = useState(true);
   const [InitialCourses, setInitialCourses] = useState([]);
   const [localLang, setLocalLang] = useState(language);
-  const [courses, setCourses] = useState([]);
   const [cardholderName, setCardholderName] = useState('');
   const [billingAddress, setBillingAddress] = useState('');
   const [zip, setZip] = useState('');
@@ -111,6 +31,9 @@ export default function PopularCourses({ language ,showCancelButton,handleNaviga
   const [plans, setPlans] = useState([]);
   const [userId, setUserId] = useState(null);
   const [confirmemail, setConfirmemail] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [date, setDate] = useState("");
+  const [cardCode, setCardCode] = useState("");
 
   useEffect(() => {
     const personId = localStorage.getItem("userId");
@@ -166,8 +89,7 @@ export default function PopularCourses({ language ,showCancelButton,handleNaviga
   const handleCancel = () => {
     setModalVisible(false);
   };
-  const stripe = useStripe();
-  const elements = useElements();
+
 
   const openNotification = (type, message) => {
     notification[type]({
@@ -217,8 +139,11 @@ function isValidEmail(email) {
       setLoading(false);
   
       // Create payment intent
-      const response = await axios.post('https://server-of-united-eldt.vercel.app/api/create-payment-intents', {
+      const response = await axios.post('https://server-of-united-eldt.vercel.app/api/create-payment-transactions', {
         amount: purchase.price,
+        cardCode,
+        cardNumber,
+        date,
         courseEnrollments: [
           {
             courseId: purchase._id,
@@ -232,48 +157,20 @@ function isValidEmail(email) {
         address: billingAddress,
         zip: zip,
       });
-  if(response.data.available === true){
-    availblemodal()
+if(response.data.available === true ){
+  availblemodal()
+  handleCancel()
+}
+      if(response.data.transactionId.transactionResponse.messages.message[0].code = 1){
+        visibleModal()
+        handleCancel()
     return
   }
-      if (response.status === 200) {
-        // Confirm payment
-        const confirmPayment = await stripe.confirmCardPayment(response.data.clientSecret, {
-          payment_method: {
-            card: elements.getElement(CardNumberElement),
-          },
-        });
-  
-        if (confirmPayment.paymentIntent.status === 'succeeded') {
-          // Payment confirmed, now create or update student
-          await axios.post("https://server-of-united-eldt.vercel.app/api/testersuccessuser", {
-            amount: purchase.price,
-            courseEnrollments: [
-              {
-                courseId: purchase._id,
-                lessonIndex: 0,
-                language: purchase.language,
-              },
-            ],
-            fullName: cardholderName,
-            Email: email,
-            price: purchase.price,
-            address: billingAddress,
-            zip: zip,
-          });
-  
-          console.log('Payment confirmed');
-          setModalVisible(false);
-          visibleModal();
-        } else {
-          throw new Error('Payment failed. Please try again.');
-        }
-      } else {
-        throw new Error('Failed to create payment intent. Please try again.');
-      }
+
     } catch (error) {
       console.error('Payment error:', error.message);
       errModal();
+      handleCancel()
       setModalVisible(false);
     } finally {
       setLoading(true);
@@ -335,111 +232,25 @@ function isValidEmail(email) {
 
 
 
+  const handleMonthYearChange = (e) => {
+    let input = e.target.value.replace(/\D/g, ''); // Remove non-numeric characters
+    let formattedDate = '';
 
+    if (input.length <= 2) {
+        formattedDate = input;
+    } else {
+        formattedDate = input.slice(0, 2) + '/' + input.slice(2);
+    }
+
+    setDate(formattedDate);
+};
   useEffect(() => {
     axios.get("https://server-of-united-eldt.vercel.app/api/courses").then((res) => {
       setPlans(res.data);
     });
   }, []);
 
-  const [activeDot, setActiveDot] = useState(0);
-  const propsSpring = useSpring({
-    opacity: 1,
-    marginTop: "0px",
-    marginBottom: "0px",
-    from: { opacity: 0 },
-  });
-  const handleBeforeChange = (oldIndex, newIndex) => {
-    setActiveDot(newIndex);
-  };
-  const CustomDropdownIndicator = () => (
-    <div className="custom-dropdown-indicator">
-      <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/expand-arrow--v1.png" alt="expand-arrow--v1" />  </div>
-  );
-
-
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplepreArrow />,
-    customPaging: (i) => (
-      <div
-        className={`custom-dot ${i === activeDot ? "active" : ""}`}
-        key={i}
-        onClick={() => setActiveDot(i)}
-        style={{
-          position: "relative",
-          bottom: "0px",
-          width: "20px",
-          height: "20px",
-          background: i === activeDot ? "#FFD87F" : "#999",
-          borderRadius: "50%",
-          margin: "0 5px",
-          cursor: "pointer",
-          marginTop: i === activeDot ? "10px" : "10px",
-          transition: "all 0.3s ease-in-out",
-        }}
-      ></div>
-    ),
-    beforeChange: handleBeforeChange,
-  };
-
-
-
-  // Responsive settings
-  const responsiveSettings = [
-    {
-      breakpoint: 2400,
-      settings: {
-        slidesToShow: 5,
-        dots: false
-      },
-    },
-    {
-      breakpoint: 1920,
-      settings: {
-        slidesToShow: 4,
-        dots: false,
-
-      },
-    },
-    {
-      breakpoint: 1440,
-      settings: {
-        slidesToShow: 3,
-        dots: false,
-
-      },
-    },
-
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-
-      },
-    },
-    {
-      breakpoint: 786,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ];
-
-  // Combine main settings with responsive settings
-  const combinedSettings = { ...settings, responsive: responsiveSettings };
-
+  
 
 
   return (
@@ -485,138 +296,12 @@ function isValidEmail(email) {
             <>
 
 
-              {/* <div className="caroselcomponent">
-                <animated.div style={propsSpring} className="container" >
-                  <Slider {...combinedSettings}>
-                    {plans.map((plan) => (
-                      <div key={plan._id} className=" card-content mx-auto" >
-                        <div className="plancard d-flex mt-2">
-                          <img src={plan.image} height="58px" alt="plan1" />
-                          <span className="flex-end">
-
-
-                            <sup className="dollar-sup">$</sup>
-                            <span className="main-price"> <Translate>{plan.price / 100}</Translate></span>
-                          </span>
-                        </div>
-
-
-                        <span className="classer">  <Translate>{plan.courseName}</Translate></span>
-                        <div className="d-flex toper">
-                          <div className="categoria">
-
-
-                            <span> <Translate>Category </Translate></span>
-                            <br></br>
-
-                            <strong className="strongcontent">  <Translate>{plan.category}</Translate></strong>
-                          </div>
-
-                        </div>
-
-                        <div className="dropdown toper" >
-                          <Translate>Select the desired language:</Translate><br></br> */}
-
-                          {/* <Select
-                            options={languageOptions}
-                            className="mt-1"
-                            menuPosition="absolute"
-                            classNamePrefix="languageSelect"
-                            onClick={() => godown()}
-                            isSearchable={false}
-                            onChange={(selectedOption) => handleLanguageChange(selectedOption, plan._id)}
-                            defaultValue={languageOptions.find((option) => option.value === 'English')}
-                            getOptionLabel={(option) => (
-                              <div className="d-flex align-items-center" style={{ height: "fit-content", zIndex: "9999" }}>
-                                <img src={option.image} alt={option.label} className="language-image" />
-                                &nbsp; {option.label}
-                              </div>
-                            )}
-                            styles={customStyles}
-                            components={{ DropdownIndicator: CustomDropdownIndicator }}
-                          /> */}
+            
                           <div className="customcard">
                                               <CustomDropdown options={languageOptions} handleLanguageChange={handleLanguageChange} language={language} plans={plans}  large={large} medium={medium} showModal={showModal} showCancelButton={showCancelButton} handleNavigationClick={handleNavigationClick}/>
 
                           </div>
-{/* <div className="buynewcourse">
-                                               <Buynew options={languageOptions} handleLanguageChange={handleLanguageChange} language={language} plans={plans} showModal={showModal}/>
-</div> */}
 
-
-
-                        {/* </div>
-
-                        <div className="toper   Acesso">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="25"
-                            viewBox="0 0 24 25"
-                            fill="none"
-                          >
-                            <path
-                              d="M12 21.8086C16.9706 21.8086 21 17.7792 21 12.8086C21 7.83803 16.9706 3.80859 12 3.80859C7.02944 3.80859 3 7.83803 3 12.8086C3 17.7792 7.02944 21.8086 12 21.8086Z"
-                              stroke="#FBB723"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                            <path
-                              d="M12 7.55859V12.8086H17.25"
-                              stroke="#FBB723"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>{" "}
-                          <span className="mx-2"><Translate>Access:</Translate><strong className="strong-text"> Unlimited</strong> </span>
-                        </div>
-                        <div className="mt-3  Acesso">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="25"
-                            viewBox="0 0 24 25"
-                            fill="none"
-                          >
-                            <path
-                              d="M12 17.3086C16.1421 17.3086 19.5 13.9507 19.5 9.80859C19.5 5.66646 16.1421 2.30859 12 2.30859C7.85786 2.30859 4.5 5.66646 4.5 9.80859C4.5 13.9507 7.85786 17.3086 12 17.3086Z"
-                              stroke="#FBB723"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                            <path
-                              d="M12 14.3086C14.4853 14.3086 16.5 12.2939 16.5 9.80859C16.5 7.32331 14.4853 5.30859 12 5.30859C9.51472 5.30859 7.5 7.32331 7.5 9.80859C7.5 12.2939 9.51472 14.3086 12 14.3086Z"
-                              stroke="#FBB723"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                            <path
-                              d="M16.5 15.8086V23.3086L12 21.0586L7.5 23.3086V15.8086"
-                              stroke="#FBB723"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>{" "}
-                          <span  className="mx-2"><Translate>TPR Certified:</Translate><strong className="strong-text"> Yes</strong> </span>
-                        </div>
-                        <button
-                          className=" buy-button "
-                          style={{ marginTop: "40px" }}
-                          onClick={() => { showModal(plan) }}
-                        >
-                          <Translate>Buy Now</Translate>
-                        </button>
-
-                      </div>
-                    ))}
-                  </Slider>
-                </animated.div>
-              </div> */}
             </>
           ) : (
             <>
@@ -711,10 +396,16 @@ function isValidEmail(email) {
               </svg> Secure</span>
             </label>
 
-            <CardNumberElement className="card-element numcard" />
+            <input className="card-element numcard" onChange={(e) => { setCardNumber(e.target.value) }} placeholder="424242424242424242" />
             <div className="d-flex">
-              <CardExpiryElement className="card-element numcard2" />
-              <CardCvcElement className="card-element numcard3" style={{ width: '100px' }} />
+            <input 
+            className="card-element numcard2" 
+            value={date} 
+            onChange={handleMonthYearChange} 
+            placeholder="MM / YY" 
+            maxLength="5" 
+        />
+              <input className="card-element numcard3" onChange={(e)=>{setCardCode(e.target.value)}} style={{ width: '100%' }} placeholder="CVV" maxLength="5"/>
             </div>
             {/* Billing address input fields */}
             <label className="labeltext">
