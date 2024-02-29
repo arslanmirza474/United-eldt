2ebb10e1f5c3b25880ff7a4af15ac4c3c09feb88
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-02-29 21:58:40 +0500

    update

--- src/Studentdashboard/Loader.jsx
+++ src/Studentdashboard/Loader.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import './Loader.css'; // Import CSS for styling
 import sitelogo from "./LOGO ELDT 2024 04 Artboard 5.svg"
 import loaderimage from "./loaderimage.png"
+import { RotatingLines } from 'react-loader-spinner';
 const Loader = () => {
   return (
     <div className='logocon'>
       <div className='maincontainer'>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="100" stroke="#f5b505" speed="0.75" data-testid="rotating-lines-svg" aria-label="rotating-lines-loading" aria-busy="true" role="progressbar" class="sc-dmyCSP cXeqit"><polyline points="24,12 24,4" width="3" transform="rotate(0, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(30, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(60, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(90, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(120, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(150, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(180, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(210, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(240, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(270, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(300, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline><polyline points="24,12 24,4" width="3" transform="rotate(330, 24, 24)" class="sc-hLQSwg jqMhbM"></polyline></svg>
+    
+    <div  style={{display:"none"}}>
+            <RotatingLines
+  visible={true}
+  height="80"
+  width="96"
+  color="#ebb31a" // Change color to orange
+  strokeWidth="3"
+  animationDuration="0.75"
+  ariaLabel="rotating-lines-loading"
+  wrapperStyle={{color:"#ebb31a"}}
+  wrapperClass=""
+/>
+    </div>
 
 
     </div>
--- src/Studentdashboard/Navba.jsx
+++ src/Studentdashboard/Navba.jsx
@@ -141,15 +141,10 @@ const handleToggle = () => {
                   onClick={closeNavbar}
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none"><path d="M2.5 5.6001V11.8501" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4.23438 17.4751C4.85974 16.5159 5.71451 15.7279 6.72128 15.1824C7.72804 14.637 8.85497 14.3513 10 14.3513C11.145 14.3513 12.272 14.637 13.2787 15.1824C14.2855 15.7279 15.1403 16.5159 15.7656 17.4751" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M17.5 5.6001L10 8.1001L2.5 5.6001L10 3.1001L17.5 5.6001Z" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M13.2266 7.02197C13.7999 7.64805 14.1787 8.4274 14.3167 9.26502C14.4548 10.1026 14.3462 10.9623 14.0042 11.7393C13.6622 12.5163 13.1015 13.1769 12.3905 13.6408C11.6795 14.1046 10.8489 14.3516 10 14.3516C9.15108 14.3516 8.3205 14.1046 7.60951 13.6408C6.89852 13.1769 6.33783 12.5163 5.99581 11.7393C5.65378 10.9623 5.54518 10.1026 5.68326 9.26502C5.82134 8.4274 6.20012 7.64805 6.77344 7.02197" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                  My courses
+                 courses
                 </Link>
               </li>
-              <li className="nav-item" onClick={gotochapterlist}>
-                <Link className="nav-link d-flex" onClick={closeNavbar}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none"><path d="M6.875 5.1001H16.875" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6.875 10.1001H16.875" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6.875 15.1001H16.875" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3.4375 6.0376C3.95527 6.0376 4.375 5.61786 4.375 5.1001C4.375 4.58233 3.95527 4.1626 3.4375 4.1626C2.91973 4.1626 2.5 4.58233 2.5 5.1001C2.5 5.61786 2.91973 6.0376 3.4375 6.0376Z" fill="#696969"></path><path d="M3.4375 11.0376C3.95527 11.0376 4.375 10.6179 4.375 10.1001C4.375 9.58233 3.95527 9.1626 3.4375 9.1626C2.91973 9.1626 2.5 9.58233 2.5 10.1001C2.5 10.6179 2.91973 11.0376 3.4375 11.0376Z" fill="#696969"></path><path d="M3.4375 16.0376C3.95527 16.0376 4.375 15.6179 4.375 15.1001C4.375 14.5823 3.95527 14.1626 3.4375 14.1626C2.91973 14.1626 2.5 14.5823 2.5 15.1001C2.5 15.6179 2.91973 16.0376 3.4375 16.0376Z" fill="#696969"></path></svg>
-                  Contents
-                </Link>
-              </li>
+           
               <li className="nav-item" onClick={gotolastchap}>
                 <a
                   className="nav-link d-flex"
@@ -159,6 +154,37 @@ const handleToggle = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none"><path d="M14.375 6.3501H3.125C2.77982 6.3501 2.5 6.62992 2.5 6.9751V15.7251C2.5 16.0703 2.77982 16.3501 3.125 16.3501H14.375C14.7202 16.3501 15 16.0703 15 15.7251V6.9751C15 6.62992 14.7202 6.3501 14.375 6.3501Z" stroke="#696969" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M5 3.8501H16.875C17.0408 3.8501 17.1997 3.91595 17.3169 4.03316C17.4342 4.15037 17.5 4.30934 17.5 4.4751V13.8501" stroke="#696969" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                   Resume
                 </a>
+              </li>
+              <li className="nav-item" onClick={gotochapterlist}>
+                <Link className="nav-link d-flex" onClick={closeNavbar}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none"><path d="M6.875 5.1001H16.875" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6.875 10.1001H16.875" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6.875 15.1001H16.875" stroke="#696969" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3.4375 6.0376C3.95527 6.0376 4.375 5.61786 4.375 5.1001C4.375 4.58233 3.95527 4.1626 3.4375 4.1626C2.91973 4.1626 2.5 4.58233 2.5 5.1001C2.5 5.61786 2.91973 6.0376 3.4375 6.0376Z" fill="#696969"></path><path d="M3.4375 11.0376C3.95527 11.0376 4.375 10.6179 4.375 10.1001C4.375 9.58233 3.95527 9.1626 3.4375 9.1626C2.91973 9.1626 2.5 9.58233 2.5 10.1001C2.5 10.6179 2.91973 11.0376 3.4375 11.0376Z" fill="#696969"></path><path d="M3.4375 16.0376C3.95527 16.0376 4.375 15.6179 4.375 15.1001C4.375 14.5823 3.95527 14.1626 3.4375 14.1626C2.91973 14.1626 2.5 14.5823 2.5 15.1001C2.5 15.6179 2.91973 16.0376 3.4375 16.0376Z" fill="#696969"></path></svg>
+                Table of Contents
+                </Link>
+              </li>
+              <li className="nav-item" onClick={gotochapterlist}>
+                <Link className="nav-link d-flex" onClick={closeNavbar}>
+                <i class="fa-solid fa-cart-arrow-down"></i>
+                                Buy Another Course 
+                </Link>
+              </li>
+              <li className="nav-item" onClick={gotochapterlist}>
+                <Link className="nav-link d-flex" onClick={closeNavbar}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
+<path d="M10 13.1001C12.7614 13.1001 15 10.8615 15 8.1001C15 5.33867 12.7614 3.1001 10 3.1001C7.23858 3.1001 5 5.33867 5 8.1001C5 10.8615 7.23858 13.1001 10 13.1001Z" stroke="#696969" stroke-width="1.5" stroke-miterlimit="10"/>
+<path d="M2.42188 17.4751C3.18979 16.1447 4.2944 15.0399 5.62465 14.2718C6.9549 13.5037 8.46392 13.0994 10 13.0994C11.5361 13.0994 13.0451 13.5037 14.3753 14.2718C15.7056 15.0399 16.8102 16.1447 17.5781 17.4751" stroke="#696969" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                                My account
+                </Link>
+              </li>
+              <li className="nav-item" onClick={gotochapterlist}>
+                <Link className="nav-link d-flex" onClick={closeNavbar}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
+  <path d="M13.5938 7.31885L16.875 10.6001L13.5938 13.8813" stroke="#696969" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M8.125 10.6001H16.875" stroke="#696969" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M8.125 17.4751H3.75C3.58424 17.4751 3.42527 17.4093 3.30806 17.292C3.19085 17.1748 3.125 17.0159 3.125 16.8501V4.3501C3.125 4.18434 3.19085 4.02537 3.30806 3.90816C3.42527 3.79095 3.58424 3.7251 3.75 3.7251H8.125" stroke="#696969" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                                Logout
+                </Link>
               </li>
             </ul>
           </div>
